68350492e5f1f4e70e9fd2a237432423d123fddd
Rifa <rifafairuzzahra@gmail.com>
2024-11-13 20:51:59 +0700

    Sudah Konversi Fragment

--- app/src/main/java/rfz/mobile/gamecorner/MainActivity.java
+++ app/src/main/java/rfz/mobile/gamecorner/MainActivity.java
@@ -1,33 +1,63 @@
 package rfz.mobile.gamecorner;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
-    private RiwayatAdapter riwayatAdapter;
-    private Button btBatalkan;
+    private BottomNavigationView bottomNavigationView;
+    private FragmentManager fm;
+    private Fragment currentFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_homepage);
+        setContentView(R.layout.activity_main);
 
-        this.btBatalkan = this.findViewById(R.id.btBatalkan);
-        this.btBatalkan.setOnClickListener(this);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        fm = getSupportFragmentManager();
+
+        currentFragment = new HomepageFragment();
+        loadFragment(new HomepageFragment());
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+
+            if (item.getItemId() == R.id.menuBeranda)
+                selectedFragment = new HomepageFragment();
+            else if (item.getItemId() == R.id.menuRiwayat)
+                selectedFragment = new DaftarRiwayatFragment();
+            else if (item.getItemId() == R.id.menuProfil)
+                selectedFragment = new ProfileFragment();
+
+            if (selectedFragment != null && currentFragment != selectedFragment) {
+                loadFragment(selectedFragment);
+            }
+
+            return true;
+        });
     }
 
-    @Override
-    public void onClick(View view) {
-//        if(view.getId() == R.id.btBatalkan){
-//            int pos = this.data.indexOf(view);
-//            this.data.remove(pos);
-//            this.kontakAdapter.notifyDataSetChanged();
-//        }
+    protected void loadFragment(Fragment fragment) {
+        FragmentTransaction ft = fm.beginTransaction();
+
+        if (currentFragment != null) {
+            ft.hide(currentFragment);
+        }
+
+        if (!fragment.isAdded()) {
+            ft.add(R.id.fragment_layout, fragment);
+        } else {
+            ft.show(fragment);
+        }
+
+        currentFragment = fragment;
+        ft.commit();
     }
 }
